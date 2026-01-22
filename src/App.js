e65fd22e8a77d078236a97b25a064ac1d97bbb3d
helena <helenasolanocastro@gmail.com>
2026-01-22 16:38:22 -0600

    Proyecto React

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import profile from './profile.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={profile} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ESTE ES MI HOLA MUNDO DE REACT - ALUMNA - HELENA SOLANO
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.utd.edu.mx"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          VISITAR EL SITIO DE LA UTD
         </a>
       </header>
     </div>
