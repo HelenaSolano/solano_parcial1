85b18044f49fd2ddf5e231f853d34d1dfab37abe
helena <helenasolanocastro@gmail.com>
2026-02-04 20:54:26 -0600

    Nuevo Proyecto React

--- src/App.js
+++ src/App.js
@@ -6,16 +6,28 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={profile} className="App-logo" alt="logo" />
-        <p>
-          ESTE ES MI HOLA MUNDO DE REACT - ALUMNA - HELENA SOLANO
-        </p>
+        <h1>
+          Evaluacion Parcial 1
+        </h1>
+        <h1>
+           Alumno(a): Solano Castro Martha Helena
+        </h1>
         <a
           className="App-link"
-          href="https://www.utd.edu.mx"
+          href="http://www.linkedin.com/in/helena-solano-0814b73a4?authuser=0"
           target="_blank"
           rel="noopener noreferrer"
         >
-          VISITAR EL SITIO DE LA UTD
+                  LINKED IN DE MI PROFILE
+        </a>
+        <br></br>
+         <a
+          className="App-link"
+          href="https://www.medikt.com.mx/practicas/documentacion.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+                  DOCUMENTACION PARCIAL 1
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import Prueba_componente from './Prueba_componente';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
